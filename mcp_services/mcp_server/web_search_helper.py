38832bb2a964ab153cb0a111219d319247cdcaaf
Sinclair Nzenwata <83012040+starksinclair@users.noreply.github.com>
2025-06-03 08:19:16 -0400

    debugging render hosting platform.

--- mcp_services/mcp_server/web_search_helper.py
+++ mcp_services/mcp_server/web_search_helper.py
@@ -36,6 +36,7 @@ class WebSearchHelper:
 
     def __init__(self) -> None:
         self.serpapi_key =  os.environ.get("SERPAPI_KEY")
+        print("SERPAPI_KEY:", self.serpapi_key, os.environ.get("SERPAPI_KEY"))
         if not self.serpapi_key:
             raise ValueError("SerpAPI key must be provided.")
         logger.info("WebSearchHelper initialized with SerpAPI key.")
